833fe2e20244103e0166ec8c3859b169332c2546
Ruhama Kassahun <140911522+Ruhi-Kass@users.noreply.github.com>
2026-03-14 14:06:31 +0300

    Add solution for Fruit Into Baskets (trial 2)

---
+++ leetcode/fruit-into-baskets_trial2.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+class Solution:
+    def totalFruit(self, fruits: list[int]) -> int:
+        if not fruits:
+            return 0
+        
+        basket = defaultdict(int)  
+        left = 0
+        max_fruits = 0
+        
+        for right in range(len(fruits)):
+            basket[fruits[right]] += 1
+            
+          
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+            
+        
+            max_fruits = max(max_fruits, right - left + 1)
+        
+        return max_fruits

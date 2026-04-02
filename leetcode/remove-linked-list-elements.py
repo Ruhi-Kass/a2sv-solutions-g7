f568d62fed26556225bd58cbb6acea9f6c574bc7
Ruhama Kassahun <140911522+Ruhi-Kass@users.noreply.github.com>
2026-04-02 15:30:31 +0300

    Implement function to remove linked list elements

---
+++ leetcode/remove-linked-list-elements.py
@@ -0,0 +1,23 @@
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+      
+        if not head:
+            return None
+        
+  
+        dummy = ListNode(0)
+        dummy.next = head
+        
+        prev = dummy
+        curr = head
+        
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+        
+        return dummy.next

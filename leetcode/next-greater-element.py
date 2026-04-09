8eadc2474201d3249e4344d18ee0ad78a2cc63b6
Ruhama Kassahun <140911522+Ruhi-Kass@users.noreply.github.com>
2026-04-09 18:19:11 +0300

    Add nextGreaterElement function in next-greater-element.py
    Implement next greater element algorithm using a stack.

---
+++ leetcode/next-greater-element.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        next_greater = {}
+        stack = []
+        
+      
+        for num in reversed(nums2):
+          
+            while stack and stack[-1] <= num:
+                stack.pop()
+            
+           
+            next_greater[num] = stack[-1] if stack else -1
+          
+            stack.append(num)
+        
+     
+        result = []
+        for num in nums1:
+            result.append(next_greater.get(num, -1))
+        
+        return result

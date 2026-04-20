ede4cf5f16c0de90596317f55a21c595fdb8b98d
Ruhama Kassahun <140911522+Ruhi-Kass@users.noreply.github.com>
2026-04-20 19:15:24 +0300

    Implement maxWidthRamp function to calculate width

---
+++ leetcode/maximum-width-ramp.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_width = 0
+        
+      
+        stack = []
+        
+      
+        for i in range(n):
+            if not stack or nums[stack[-1]] > nums[i]:
+                stack.append(i)
+        
+      
+        for j in range(n - 1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[j]:
+                i = stack.pop()
+                max_width = max(max_width, j - i)
+        
+        return max_width
